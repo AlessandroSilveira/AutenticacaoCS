f1b50de49e97244517fe41e2a6b1bfc22b5fa0d3
alessandro.silveira <alessandro.silveira@concrete.com.br>
2017-05-08 09:25:53 -0300

    Commit 08052017

--- Autenticacao.API/Models/Login.cs
+++ Autenticacao.API/Models/Login.cs
@@ -1,8 +1,16 @@
 ﻿namespace Autenticacao.API.Models
 {
+<<<<<<< HEAD
     public class Login
     {
         public string Email { get; set; }
         public string Senha { get; set; }
     }
+=======
+	public class Login
+	{
+		public string Email { get; set; }
+		public string Senha { get; set; }
+	}
+>>>>>>> Commit 08052017
 }
